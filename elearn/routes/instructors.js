61830d08ec277e9d542313ab7213affe90520d4a
Esteban Mendez <mendez01@allegheny.edu>
2023-10-02 10:14:04 -0400

    trying new things to get the edit button to work

--- elearn/routes/instructors.js
+++ elearn/routes/instructors.js
@@ -52,5 +52,39 @@ router.post('/classes/:id/lessons/new', function(req, res, next){
 	res.redirect('/instructors/classes');
 });
 
+// GET request to render the lesson as raw HTML for editing
+router.get('/classes/:classId/lessons/edit/:lessonId', function(req, res, next){
+    Lesson.getLessonById(req.params.lessonId, function(err, lesson){
+        if (err) throw err;
+
+        // Render the lesson as raw HTML for editing
+        res.render('instructors/editlesson', { lesson });
+    });
+});
+
+// POST request to update the lesson content
+router.post('/classes/:classId/lessons/update/:lessonId', function(req, res, next){
+    // Retrieve the lesson by lessonId
+    Lesson.getLessonById(req.params.lessonId, function(err, lesson){
+        if (err) throw err;
+
+        // Update lesson content with the data from the form
+        lesson.lesson_number = req.body.lesson_number;
+        lesson.lesson_title = req.body.lesson_title;
+        lesson.lesson_body = req.body.lesson_body;
+        lesson.lesson_code = req.body.lesson_code;
+
+        // Save the updated lesson
+        lesson.save(function(err) {
+            if (err) throw err;
+
+            // Redirect to the lesson view page or another appropriate location
+            res.redirect('/classes/' + lesson.class_id + '/lessons');
+        });
+    });
+});
+
+
+
 
 module.exports = router;
