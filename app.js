ed2ddd6842028ab2f3761a3fb28a02610cfb1f95
Esteban Mendez <mendez01@allegheny.edu>
2023-10-16 14:16:55 -0400

    Final update prototype

--- app.js
+++ app.js
@@ -1,129 +1,130 @@
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const exphbs = require('express-handlebars')
-const expressValidator = require('express-validator')
-const flash = require('connect-flash')
-const session = require('express-session')
-const passport = require('passport')
-const LocalStrategy = require('passport-local'); let Strategy
-const mongo = require('mongodb')
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/elearn')
-const db = mongoose.connection
-async = require('async')
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const async = require('async');
+const crypto = require('crypto'); // Required for generating a session secret
+require('dotenv').config(); // Load environment variables from a .env file
 
-const routes = require('./routes/index')
-const users = require('./routes/users')
-const us = require('./routes/us')
-const classes = require('./routes/classes')
-const students = require('./routes/students')
-const instructors = require('./routes/instructors')
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const us = require('./routes/us');
+const classes = require('./routes/classes');
+const students = require('./routes/students');
+const instructors = require('./routes/instructors');
 
-const app = express()
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.engine('handlebars', exphbs({ defaultLayout: 'layout' }))
-app.set('view engine', 'handlebars')
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'layout' }));
+app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+// Middleware and configurations
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Express Session
+const sessionSecret = crypto.randomBytes(64).toString('hex'); // Generate a session secret
 app.use(session({
-  secret: 'secret',
+  secret: sessionSecret,
   saveUninitialized: true,
   resave: true
-}))
+}));
 
 // Passport
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express Validator
 app.use(expressValidator({
   errorFormatter: function (param, msg, value) {
-    const namespace = param.split('.')
-    const root = namespace.shift()
-    let formParam = root
+    const namespace = param.split('.');
+    const root = namespace.shift();
+    let formParam = root;
 
     while (namespace.length) {
-      formParam += '[' + namespace.shift() + ']'
+      formParam += '[' + namespace.shift() + ']';
     }
     return {
       param: formParam,
       msg,
       value
-    }
+    };
   }
-}))
+}));
 
 // Connect-Flash
-app.use(flash())
+app.use(flash());
 
 // Makes the user object global in all views
 app.get('*', function (req, res, next) {
-  // put user into res.locals for easy access from templates
-  res.locals.user = req.user || null
+  // Put user into res.locals for easy access from templates
+  res.locals.user = req.user || null;
   if (req.user) {
-    res.locals.type = req.user.type
+    res.locals.type = req.user.type;
   }
-  next()
-})
+  next();
+});
 
 // Global Vars
 app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.error_msg = req.flash('error_msg')
-  res.locals.error = req.flash('error')
-  next()
-})
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
-app.use('/', routes)
-app.use('/users', users)
-app.use('/us', us)
-app.use('/classes', classes)
-app.use('/students', students)
-app.use('/instructors', instructors)
+// MongoDB Connection
+mongoose.connect(`mongodb+srv://mendez01:${process.env.MONGO_PASSWORD}@cluster0.zsvww85.mongodb.net/elearn`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  authMechanism: 'DEFAULT'
+});
 
-// catch 404 and forward to error handler
+// Routes
+app.use('/', routes);
+app.use('/users', users);
+app.use('/us', us);
+app.use('/classes', classes);
+app.use('/students', students);
+app.use('/instructors', instructors);
+
+// Error handling middleware
 app.use(function (req, res, next) {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
-    res.status(err.status || 500)
+    res.status(err.status || 500);
     res.render('error', {
       message: err.message,
       error: err
-    })
-  })
+    });
+  });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500)
+  res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
-  })
-})
+  });
+});
 
-module.exports = app
+module.exports = app;
--- routes/classes.js
+++ routes/classes.js
@@ -1,85 +1,101 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const Class = require('../models/class')
+const Class = require('../models/class');
 
 // Classes Page
 router.get('/', function (req, res, next) {
   Class.getClasses(function (err, classes) {
-    if (err) throw err
-    res.render('classes/index', { classes })
-  }, 3)
-})
+    if (err) throw err;
+    res.render('classes/index', { classes });
+  }, 3);
+});
 
 // Class Details
 router.get('/:id/details', function (req, res, next) {
-  Class.getClassById([req.params.id], function (err, classname) {
-    if (err) throw err
-    res.render('classes/details', { class: classname })
-  })
-})
+  Class.getClassById(req.params.id, function (err, classname) {
+    if (err) throw err;
+    res.render('classes/details', { class: classname });
+  });
+});
 
 // Get Lessons
 router.get('/:id/lessons', function (req, res, next) {
-  Class.getClassById([req.params.id], function (err, classname) {
-    if (err) throw err
-    res.render('classes/lessons', { class: classname })
-  })
-})
+  Class.getClassById(req.params.id, function (err, classname) {
+    if (err) throw err;
+    res.render('classes/lessons', { class: classname });
+  });
+});
 
 // Get Lesson
 router.get('/:id/lessons/:lesson_id', function (req, res, next) {
-  Class.getClassById([req.params.id], function (err, classname) {
-    let lesson
-    if (err) throw err
-    for (i = 0; i < classname.lessons.length; i++) {
+  Class.getClassById(req.params.id, function (err, classname) {
+    let lesson;
+    if (err) throw err;
+    for (let i = 0; i < classname.lessons.length; i++) {
       if (classname.lessons[i].lesson_number == req.params.lesson_id) {
-        lesson = classname.lessons[i]
+        lesson = classname.lessons[i];
+        break;
       }
     }
-    res.render('classes/lesson', { class: classname, lesson })
-  })
-})
+    res.render('classes/lesson', { class: classname, lesson });
+  });
+});
 
 // GET request to render the lesson as raw HTML for editing
-router.get('/:id/lessons/:lesson_id', function (req, res, next) {
-  Class.getClassById([req.params.id], function (err, classname) {
-    let lesson
-    if (err) throw err
-    for (i = 0; i < classname.lessons.length; i++) {
+router.get('/:id/lessons/:lesson_id/edit', function (req, res, next) {
+  Class.getClassById(req.params.id, function (err, classname) {
+    let lesson;
+    if (err) throw err;
+    for (let i = 0; i < classname.lessons.length; i++) {
       if (classname.lessons[i].lesson_number == req.params.lesson_id) {
-        lesson = classname.lessons[i]
+        lesson = classname.lessons[i];
+        break;
       }
     }
-    res.render('classes/editlesson', { class: classname, lesson })
-  })
-})
+    res.render('classes/editlesson', { class: classname, lesson });
+  });
+});
 
 // POST request to update the lesson content
-router.post('/:id/lessons/:lesson_id', function (req, res, next) {
-  // Retrieve the lesson by lessonId
-  Class.getClassById([req.params.id], function (err, lesson) {
-    var lesson
-    if (err) throw err
+router.post('/:id/lessons/:lesson_number/edit', function (req, res, next) {
+  // Retrieve the class by classId
+  Class.getClassById(req.params.id, function (err, classname) {
+    if (err) throw err;
 
-    // Update lesson content with the data from the form
-    lesson.lesson_number = req.body.lesson_number
-    lesson.lesson_title = req.body.lesson_title
-    lesson.lesson_body = req.body.lesson_body
-    lesson.lesson_body2 = req.body.lesson_body2
-    lesson.lesson_body3 = req.body.lesson_body3
-    lesson.lesson_code = req.body.lesson_code
-    lesson.lesson_code2 = req.body.lesson_code2
-    lesson.lesson_code3 = req.body.lesson_code3
+    // Find the lesson by lesson_number
+    let lesson;
+    for (let i = 0; i < classname.lessons.length; i++) {
+      if (classname.lessons[i].lesson_number == req.params.lesson_number) {
+        lesson = classname.lessons[i];
+        break;
+      }
+    }
 
-    // Save the updated lesson
-    lesson.save(function (err) {
-      if (err) throw err
+    if (!lesson) {
+      // Handle the case where the lesson is not found
+      // You can customize this part to display an error message or handle it as needed.
+      res.redirect('/classes/' + req.params.id + '/lessons');
+    } else {
+      // Update lesson content with the data from the form
+      lesson.lesson_title = req.body.lesson_title;
+      lesson.lesson_body = req.body.lesson_body;
+      lesson.lesson_body2 = req.body.lesson_body2;
+      lesson.lesson_body3 = req.body.lesson_body3;
+      lesson.lesson_code = req.body.lesson_code;
+      lesson.lesson_code2 = req.body.lesson_code2;
+      lesson.lesson_code3 = req.body.lesson_code3;
 
-      // Redirect to the lesson view page or another appropriate location
-      res.redirect('/classes/' + lesson.class_id + '/lessons')
-    })
-  })
-})
+      // Save the updated lesson to the database
+      classname.save(function (err) {
+        if (err) throw err;
 
-module.exports = router
+        // Redirect to the lessons page after successfully updating the lesson
+        res.redirect('/classes/' + req.params.id + '/lessons');
+      });
+    }
+  });
+});
+
+
+module.exports = router;
